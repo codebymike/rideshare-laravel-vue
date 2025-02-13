c81eb9f3938f181de2082e7f4bb7b1d70b4b3832
Mike Thomas <mikethomas84@gmail.com>
2025-02-13 12:41:39 +0000

    login route + controller

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+use App\Http\Controllers\LoginController;
+
+Route::post('/login', [LoginController::class, 'submit']);
