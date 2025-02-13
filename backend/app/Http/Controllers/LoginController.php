ad22f3ee98cbc6d9e4b3ac776328c2a9eab0839d
Mike Thomas <mikethomas84@gmail.com>
2025-02-13 12:48:58 +0000

    login controller:submit

--- backend/app/Http/Controllers/LoginController.php
+++ backend/app/Http/Controllers/LoginController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    //
+    public function submit(Request $request)
+    {
+        $request->validate([
+            'phone' => 'required|numeric|min:10'
+        ]);
+
+        $user = User::firstOrCreate([
+            'phone' => $request->phone
+        ]);
+
+        if (!$user) {
+            return response()->json(['message' => 'Could not process a user with that phone number.'], 401);
+        }
+
+        // send the user a one-time use code
+        $user->notify(new LoginNeedsVerification());
+
+        return response()->json(['message' => 'Text message notification sent.']);
+    }
 }
