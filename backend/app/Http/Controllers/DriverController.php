17e9021516c1cbfc4ae53fc914df78062454495c
Mike Thomas <mikethomas84@gmail.com>
2025-02-14 11:01:39 +0000

    driver show method

--- backend/app/Http/Controllers/DriverController.php
+++ backend/app/Http/Controllers/DriverController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class DriverController extends Controller
 {
-    //
+    public function show(Request $request)
+    {
+        // return back the user and associated driver model
+        $user = $request->user();
+        $user->load('driver');
+
+        return $user;
+    }
 }
