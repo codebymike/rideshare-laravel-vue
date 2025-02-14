90112394b12e8aa5be049cd4eba0ef63e007351e
Mike Thomas <mikethomas84@gmail.com>
2025-02-14 11:07:40 +0000

    trip controller added

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -10,9 +10,14 @@ Route::post('/login/verify', [LoginController::class, 'verify']);
 
 Route::group(['middleware' => 'auth:sanctum'], function () {
 
+    // Driver routes
     Route::get('/driver', [DriverController::class, 'show']);
     Route::post('/driver', [DriverController::class, 'update']);
 
+    // Trip routes
+    Route::post('/trip', [TripController::class, 'store']);
+
+    // User routes
     Route::get('/user', function(Request $request) {
         return $request->user();
     });
