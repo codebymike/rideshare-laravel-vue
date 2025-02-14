62a3072287c66663995078e3e23e8df006335513
Mike Thomas <mikethomas84@gmail.com>
2025-02-14 11:10:42 +0000

    trip store route

--- backend/app/Http/Controllers/TripController.php
+++ backend/app/Http/Controllers/TripController.php
@@ -6,5 +6,20 @@ use Illuminate\Http\Request;
 
 class TripController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $request->validate([
+            'origin' => 'required',
+            'destination' => 'required',
+            'destination_name' => 'required'
+        ]);
+
+        $trip = $request->user()->trips()->create($request->only([
+            'origin',
+            'destination',
+            'destination_name'
+        ]));
+
+        return $trip;
+    }
 }
