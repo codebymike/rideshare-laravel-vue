cebba8dfd0617c9af2ec690d119e716a598f66c2
Mike Thomas <mikethomas84@gmail.com>
2025-02-22 17:51:07 +0000

    app cleaned up

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,7 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-
-    <h1 class="bg-green-100">Hello</h1>
-
-  </header>
-
   <RouterView />
 </template>
-
-<style scoped>
-</style>
