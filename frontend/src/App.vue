67e7d9a99917cb464be708b5dbbb9741f34334c0
Mike Thomas <mikethomas84@gmail.com>
2025-02-23 16:21:58 +0000

    enter phone form showing

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,7 +3,7 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div class="min-h-sreen bg-gray-100 text-center antialiased">
+  <div class="min-h-screen bg-gray-100 text-center antialiased">
     <RouterView />
   </div>
 </template>
