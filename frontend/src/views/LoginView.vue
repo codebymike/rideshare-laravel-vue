e617599a40c0281ca0872ee347b63268e25581e1
Mike Thomas <mikethomas84@gmail.com>
2025-02-24 13:38:16 +0000

    verification form switcher

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,7 +1,9 @@
 <script setup>
     import { vMaska } from "maska/vue"
-    import { reactive } from 'vue'
+    import { ref, reactive } from 'vue'
     import axios from 'axios'
+
+    const waitingOnVerification = ref(false)
 
     const credentials = reactive({
         phone: null,
@@ -17,6 +19,7 @@
         axios.post('http://localhost/api/login', getFormattedCredentials())
         .then((response) => {
             console.log(response.data)
+            waitingOnVerification.value = true
         })
         .catch((error) => {
             console.error(error)
@@ -28,7 +31,23 @@
 <template>
     <div class="pt-16">
         <h1 class="text-3xl font-semibold mb-4">Enter your phone number</h1>
-        <form action="#" @submit.prevent="handleLogin">
+
+        <form v-if="!waitingOnVerification" action="#" @submit.prevent="handleLogin">
+            <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
+                <div class="bg-white px-4 py-5 sm:p-6">
+                    <div>
+                        <input type="text" v-maska data-maska="# (###) ###-####" v-model="credentials.phone" name="phone" id="phone" placeholder="1 (234) 567-8910"
+                            class="mt-1 block w-full px-3 py-2 rounded-md border border-gray-300 shadow-sm focus:border-black focus:outline-none">
+                    </div>
+                </div>
+                <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
+                    <button type="submit"
+                        class="inline-flex justify-center rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none">Continue</button>
+                </div>
+            </div>
+        </form>
+
+        <form v-else action="#" @submit.prevent="handleLogin">
             <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
                 <div class="bg-white px-4 py-5 sm:p-6">
                     <div>
