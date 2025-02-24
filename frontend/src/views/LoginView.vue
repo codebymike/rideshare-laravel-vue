fcfe38a7f0fa64c4281cd75026da088827dea71e
Mike Thomas <mikethomas84@gmail.com>
2025-02-24 13:40:33 +0000

    verif state handling

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -7,11 +7,13 @@
 
     const credentials = reactive({
         phone: null,
+        login_code: null
     })
 
     const getFormattedCredentials = () => {
         return {
-            phone: credentials.phone.replace(/\D/g,'')
+            phone: credentials.phone.replace(/\D/g,''),
+            login_code: credentials.login_code
         }
     }
 
@@ -47,17 +49,17 @@
             </div>
         </form>
 
-        <form v-else action="#" @submit.prevent="handleLogin">
+        <form v-else action="#" @submit.prevent="handleVerification">
             <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
                 <div class="bg-white px-4 py-5 sm:p-6">
                     <div>
-                        <input type="text" v-maska data-maska="# (###) ###-####" v-model="credentials.phone" name="phone" id="phone" placeholder="1 (234) 567-8910"
+                        <input type="text" v-maska data-maska="######" v-model="credentials.login_code" name="login_code" id="login_code" placeholder="123456"
                             class="mt-1 block w-full px-3 py-2 rounded-md border border-gray-300 shadow-sm focus:border-black focus:outline-none">
                     </div>
                 </div>
                 <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
-                    <button type="submit"
-                        class="inline-flex justify-center rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none">Continue</button>
+                    <button type="submit" @submit.prevent="handleVerification"
+                        class="inline-flex justify-center rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none">Verify</button>
                 </div>
             </div>
         </form>
