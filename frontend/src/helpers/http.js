756dabe5b9ea6c55b52fc57b7f7af6990501b64a
Mike Thomas <mikethomas84@gmail.com>
2025-02-27 13:36:09 +0000

    api helper

---
+++ frontend/src/helpers/http.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const http = () => {
+  let options = {
+    baseURL: import.meta.env.VITE_API_URL,
+    headers: {},
+  };
+
+  if (localStorage.getItem("token")) {
+    options.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
+  }
+
+  return axios.create(options);
+};
+
+export default http;
