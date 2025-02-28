f71ce22fddf823e48b566c808be95d560dbd9b92
Mike Thomas <mikethomas84@gmail.com>
2025-02-28 14:13:08 +0000

    standby + driver views

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
+import axios from "axios";
 import LoginView from "@/views/LoginView.vue";
 import LandingView from "@/views/LandingView.vue";
 import LocationView from "@/views/LocationView.vue";
 import MapView from "@/views/MapView.vue";
 import TripView from "@/views/TripView.vue";
-import axios from "axios";
+import StandbyView from "@/views/StandbyView.vue";
+import DriverView from "@/views/DriverView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -33,6 +35,16 @@ const router = createRouter({
       path: "/trip",
       name: "trip",
       component: TripView,
+    },
+    {
+      path: "/standby",
+      name: "standby",
+      component: StandbyView,
+    },
+    {
+      path: "/driver",
+      name: "driver",
+      component: DriverView,
     },
   ],
 });
---
+++ frontend/src/views/StandbyView.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+</script>
+<template>
+    <div class="pt-16">
+        <h1 class="text-3xl font-semibold mb-4">title</h1>
+        <div>
+            <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
+                <div class="bg-white px-4 py-5 sm:p-6">
+                    <div>
+                        Gmap
+                    </div>
+                    <div class="mt-2">
+                        <p class="text-xl">Going to <strong>trip.destination_name</strong></p>
+                    </div>
+                </div>
+                <div class="flex justify-between bg-gray-50 px-4 py-3 text-right sm:px-6">
+                    <button
+                        @click="handleDeclineTrip"
+                        class="inline-flex justify-center rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none">Decline</button>
+                    <button
+                        @click="handleAcceptTrip"
+                        class="inline-flex justify-center rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none">Accept</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
