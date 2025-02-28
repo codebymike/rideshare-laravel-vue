c947e77eadfa8828b1ae6b792f802bca7702b66c
Mike Thomas <mikethomas84@gmail.com>
2025-02-28 13:43:30 +0000

    trip view init

---
+++ frontend/src/views/TripView.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+<template>
+    <div class="pt-16">
+        <h1 class="text-3xl font-semibold mb-4">Title</h1>
+        <div>
+            <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
+                <div class="bg-white px-4 py-5 sm:p-6">
+                    <div>
+                        GMap
+                    </div>
+                </div>
+                <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
+                    <span>message</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
