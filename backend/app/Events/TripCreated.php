2100e5810e61dd71ca033478e0fa7e0d32cabeb5
Mike Thomas <mikethomas84@gmail.com>
2025-03-17 14:20:16 +0000

    trip created event

--- backend/app/Events/TripCreated.php
+++ backend/app/Events/TripCreated.php
@@ -12,7 +12,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class TripStarted implements ShouldBroadcast
+class TripCreated implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
@@ -36,7 +36,7 @@ class TripStarted implements ShouldBroadcast
     public function broadcastOn(): array
     {
         return [
-            new Channel('passenger_' . $this->user->id)
+            new Channel('drivers')
         ];
     }
 }
