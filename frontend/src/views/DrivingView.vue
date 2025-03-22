e4f7d8e3066140a2020559a307f7e4b6d5c022a7
Mike Thomas <mikethomas84@gmail.com>
2025-03-22 19:05:36 +0000

    clear driving template

--- frontend/src/views/DrivingView.vue
+++ frontend/src/views/DrivingView.vue
@@ -5,7 +5,7 @@
     <div class="pt-16">
         <h1 class="text-3xl font-semibold mb-4"> title </h1>
         <div>
-            <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left" v-if="!trip.is_complete">
+            <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
                 <div class="bg-white px-4 py-5 sm:p-6">
                     <div>
                         Gmap
@@ -15,19 +15,7 @@
                     </div>
                 </div>
                 <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
-                    <button v-if="trip.is_started"
-                        @click="handleCompleteTrip"
-                        class="inline-flex justify-center rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none">
-                        Complete Trip</button>
-                    <button v-else
-                        @click="handlePassengerPickedUp"
-                        class="inline-flex justify-center rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none">
-                        Passenger Picked Up</button>
-                </div>
-            </div>
-            <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left" v-else>
-                <div class="bg-white px-4 py-5 sm:p-6">
-                    <Tada />
+
                 </div>
             </div>
         </div>
