d53af45c7d800688466819ec736203894e7d52b4
Mike Thomas <mikethomas84@gmail.com>
2025-03-23 22:08:11 +0000

    trip view gmap

--- frontend/src/views/TripView.vue
+++ frontend/src/views/TripView.vue
@@ -8,7 +8,11 @@
             <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
                 <div class="bg-white px-4 py-5 sm:p-6">
                     <div>
-                        GMap
+                        <GMapMap :zoom="14" :center="location.current.geometry" ref="gMap"
+                            style="width:100%; height: 256px;">
+                            <GMapMarker :position="location.current.geometry" :icon="currentIcon" />
+                            <GMapMarker :position="trip.driver_location" :icon="driverIcon" />
+                        </GMapMap>
                     </div>
                 </div>
                 <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
