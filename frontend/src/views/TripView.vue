a330c1e29bd0152c3fc70db67a137a602fbbf345
Mike Thomas <mikethomas84@gmail.com>
2025-03-23 22:14:37 +0000

    title and message refs

--- frontend/src/views/TripView.vue
+++ frontend/src/views/TripView.vue
@@ -8,6 +8,9 @@ import Pusher from 'pusher-js'
 const trip = useTripStore()
 const location = useLocationStore()
 
+const title = ref('Waiting on a driver...')
+const message = ref('When a driver accepts the trip, their info will appear here.')
+
 const currentIcon = {
     url: 'https://openmoji.org/data/color/svg/1F920.svg',
     scaledSize: {
@@ -48,7 +51,7 @@ onMounted(() => {
 </script>
 <template>
     <div class="pt-16">
-        <h1 class="text-3xl font-semibold mb-4">Title</h1>
+        <h1 class="text-3xl font-semibold mb-4">{{ title }}</h1>
         <div>
             <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
                 <div class="bg-white px-4 py-5 sm:p-6">
@@ -61,7 +64,7 @@ onMounted(() => {
                     </div>
                 </div>
                 <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
-                    <span>message</span>
+                    <span>{{ message }}</span>
                 </div>
             </div>
         </div>
