14ac82764741d577f1708f96c88180952c4edb02
Mike Thomas <mikethomas84@gmail.com>
2025-03-26 15:27:01 +0000

    vif animation

--- frontend/src/views/DrivingView.vue
+++ frontend/src/views/DrivingView.vue
@@ -109,7 +109,7 @@ const broadcastDriverLocation = () => {
     <div class="pt-16">
         <h1 class="text-3xl font-semibold mb-4">{{ title }}</h1>
         <div>
-            <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
+            <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left" v-if="!trip.is_complete">
                 <div class="bg-white px-4 py-5 sm:p-6">
                     <div>
                         <GMapMap :zoom="14" :center="location.current.geometry" ref="gMap"
@@ -131,6 +131,11 @@ const broadcastDriverLocation = () => {
                         @click="handlePassengerPickedUp"
                         class="inline-flex justify-center rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none">
                         Passenger Picked Up</button>
+                </div>
+            </div>
+            <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left" v-else>
+                <div class="bg-white px-4 py-5 sm:p-6">
+                    <Tada />
                 </div>
             </div>
         </div>
